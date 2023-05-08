7011ec471605c290c675c7859688a4a5875ca03d
Changseo Jang <changseo.jang@riiid.co>
2023-05-08 12:53:59 +0900

    feat: separate dotfiles list

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,13 +3,15 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/index.tsx";
-import * as $1 from "./routes/install.ts";
+import * as $0 from "./routes/dotfiles.tsx";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/install.ts";
 
 const manifest = {
   routes: {
-    "./routes/index.tsx": $0,
-    "./routes/install.ts": $1,
+    "./routes/dotfiles.tsx": $0,
+    "./routes/index.tsx": $1,
+    "./routes/install.ts": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,64 +1,10 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
-import { GraphQLRequest } from "gql_request";
-import { githubGqlEndpoint } from "../utils/constant.ts";
-import { getEnv } from "../utils/env.ts";
-import RepositoryCard from "../components/RepositoryCard.tsx";
-import { Repository } from "../types/Repository.ts";
 import { FunctionComponent } from "preact";
 import Layout from "../components/layout/Layout.tsx";
 
-export const handler: Handlers<Repository[] | null> = {
-  async GET(_, ctx) {
-    const query = await Deno.readTextFile(
-      "./queries/listRepositoryInTopic.gql",
-    );
-    const request = new GraphQLRequest(
-      githubGqlEndpoint,
-      query,
-      {
-        headers: {
-          Authorization: `Bearer ${getEnv("GITHUB_TOKEN")}`,
-        },
-        variables: {
-          first: 15,
-        },
-      },
-    );
-
-    const response = await fetch(request);
-    const body: {
-      data: {
-        topic: {
-          name: string;
-          repositories: { edges: { node: Repository }[] };
-        };
-      };
-    } = await response.json();
-
-    return ctx.render(
-      body.data.topic.repositories.edges.map((elem) => elem.node),
-    );
-  },
-};
-
-const home: FunctionComponent<PageProps<Repository[] | null>> = (
-  { data },
-) => {
-  if (!data) {
-    return <h1>Failed to fetch data</h1>;
-  }
-
+const home: FunctionComponent = () => {
   return (
     <Layout>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <p class="my-6">
-          {data?.map((elem) => (
-            <RepositoryCard
-              repo={elem}
-            />
-          ))}
-        </p>
-      </div>
+      Home
     </Layout>
   );
 };
